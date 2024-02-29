cb5c8d47001ed9616e1f8966e0fde0ced2dee824
Hironori Ishikawa <ishikawahiroyuki@Ishikawa-hironori-MacBook-Pro.local>
2024-02-29 14:07:38 +0900

    message

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    domains: ['ibtfehfnrqovkywmypaz.supabase.co'],
+  },
+};
 
 export default nextConfig;
