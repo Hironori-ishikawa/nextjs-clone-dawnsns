7093642bdfcb30ac79930a1a3e42b214133ecbe7
Hironori Ishikawa <ishikawahiroyuki@Ishikawa-hironori-MacBook-Pro.local>
2024-03-04 14:51:47 +0900

    message

--- src/app/components/navigation.tsx
+++ src/app/components/navigation.tsx
@@ -1,10 +1,32 @@
 'use client'
 
 import Link from 'next/link'
+import useStore from '../../../store'
+import Image from 'next/image'
+import { useEffect } from 'react'
 import type { Session } from '@supabase/auth-helpers-nextjs'
+import { Database } from '../../../lib/database.types'
+type ProfileType = Database['public']['Tables']['profiles']['Row']
 
 // ナビゲーション
-const Navigation = ({ session }: { session: Session | null }) => {
+const Navigation = ({ session, profile }: {
+  session: Session | null
+  profile: ProfileType | null
+}) => {
+  const { setUser } = useStore()
+
+  // 状態管理にユーザー情報を保存
+  useEffect(() => {
+    setUser({
+      id: session ? session.user.id : '',
+      email: session ? session.user.email! : '',
+      name: session && profile ? profile.name : '',
+      introduce: session && profile ? profile.introduce : '',
+      avatar_url: session && profile ? profile.avatar_url : '',
+    })
+  }, [session, setUser, profile])
+
+
   return (
     <header className='shadow-lg shadow-gray-100'>
       <div className='py-5 container max-w-screen-sm mx-auto flex items-center justify-between'>
@@ -14,13 +36,20 @@ const Navigation = ({ session }: { session: Session | null }) => {
 
         <div className='text-sm font-bold'>
           {session ? (
-            <link href="/settings/profile">
-              <div>プロフィール</div>
-            </link>
+            <Link href="/settings/profile">
+              <div className='relative w-10 h-10'>
+                <Image
+                  src={profile && profile.avatar_url ? profile.avatar_url : '/default.png'}
+                  className='rounded-full object-cover'
+                  alt='avatar'
+                  fill
+                />
+              </div>
+            </Link>
           ) : (
             <div className='flex items-center space-x-5'>
               <Link href="/auth/login">ログイン</Link>
-              <Link href="auth/signup">サインアップ</Link>
+              <Link href="/auth/signup">サインアップ</Link>
             </div>
           )}
 
